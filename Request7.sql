02611de976396b3c4c796287d0ec2bb04bc4cd93
Estebge <estebanbegue7@icloud.com>
2024-11-19 00:24:27 +0100

    Create Request7.sql

---
+++ Request7.sql
@@ -0,0 +1,9 @@
+SELECT report_name
+FROM report
+INNER JOIN give ON report.id_report = give.id_report
+INNER JOIN record ON give.id_record = record.id_record
+INNER JOIN sensor ON record.id_sensor = sensor.id_sensor
+INNER JOIN gaz ON sensor.id_gaz = gaz.id_gaz
+WHERE gaz.gaz_name = 'NH3'
+GROUP BY report.report_name, report.date_report
+ORDER BY report.date_report;
