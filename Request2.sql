9acadad9eba600db1c55ba3aebcadc2ac1a13d20
Estebge <estebanbegue7@icloud.com>
2024-11-19 00:22:35 +0100

    Create Request2.sql

---
+++ Request2.sql
@@ -0,0 +1,4 @@
+SELECT agent.fn_agent, agent.sn_agent FROM agent
+INNER JOIN agency ON agency.id_agency = agent.id_agency
+INNER JOIN city ON city.id_city = agency.id_city
+WHERE city_name = 'Bordeaux' AND job_name = 'TECH' ;
